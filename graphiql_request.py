3a61fb33d4da25a15c1f33c15e89de3f46f2617b
katkoler <katjusa.koler@gmail.com>
2017-12-02 16:34:08 +0000

    get graphql git bios

--- graphiql_request.py
+++ graphiql_request.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
 import my_git_token
+import requests
+import json
 
 token = my_git_token.my_token()
+pass1 = my_git_token.pass1()
+
+headers = {'Authorization':'Bearer '+token}
+print(headers)
+
+query = '{ repository(owner: "paulienuh", name: "25DaysOfGiphiness") { collaborators(first: 20) {edges {node {name login bio bioHTML }}}}}'
+
+
+
+r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
+
+
+git_bio = (r.json())
+
+with open('git_bio.json', 'w') as outfile:
+	json.dump(git_bio, outfile)
