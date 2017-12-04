f26e533425010b49f64a07a8f5fefc43305bac9b
Darren Vong <d.vongy@gmail.com>
2017-12-04 23:47:45 +0000

    Multiple santas (up to 5, aka part of #5) can now be launched 🎉

--- static/index.js
+++ static/index.js
@@ -1,8 +1,11 @@
 $(() => {
-    let date = new Date();
-    let d = date.getDate();
-    
-    let message_list = ['Not yet dummy 😝', 'Wait for it...👀', 'Take the hint! 🙃', 'I said NO!😶', 'This is random 😵', 'You will be put on the bad list! 🙄', 'Santa is not coming to town ... yet 😛', 'Try tommorrow... 😱', 'Enter password: 🔐', 'Few more days! 💩']
+    const date = new Date();
+    const d = date.getDate();   
+    const message_list = ['Not yet dummy 😝', 'Wait for it...👀', 'Take the hint! 🙃',
+                          'I said NO!😶', 'This is random 😵', 'You will be put on the bad list! 🙄',
+                          'Santa is not coming to town ... yet 😛', 'Try tommorrow... 😱',
+                          'Enter password: 🔐', 'Few more days! 💩'];
+
     for (let i = 1; i <= 25; i++) {
         $(`.day${i}`).click(function() {
             if (i <= parseInt(d)) {
@@ -16,11 +19,18 @@ $(() => {
     }
 
     $("html").click(() => {
-        let $santaDiv = $(".santa_div"),
+        const MAX_SANTAS = 5;
+        let $santaDiv = $(".santa_div").first(),
+            numOfSantas = $(".santa_div").length,
             $santaClone = $santaDiv.clone(true);
         
-        $santaDiv.before($santaClone);
-
-        $(`.${$santaDiv.attr("class")}:last`).remove();
+        if (numOfSantas < MAX_SANTAS) {
+            // Make more santas only if we're currently under the limit
+            $santaDiv.before($santaClone);
+            // Clean up the santa once they've finished animating
+            $santaClone.on("animationend", function() {
+                $(this).remove();
+            });
+        }
     });
 });
